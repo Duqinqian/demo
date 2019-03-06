5064f2b071e1109986732c67f43a97f49113b05f
Qin <1012674963@qq.com>
2019-03-06 16:18:21 +0800

    [mod] er

--- src/components/tabBar/tabBar.vue
+++ src/components/tabBar/tabBar.vue
@@ -20,9 +20,10 @@
 				isActive:this.$store.state.tabIndex,
 				itemList:[
 					{text:"推荐",icon:"fa fa-clipboard",link:'/'},
-					{text:"作品",icon:"fa fa-smile-o",link:'/things'},
-					{text:"设计师",icon:"fa fa-pencil",link:'/designer'},
-					{text:"我",icon:"fa fa-user-o",link:'/personal'}
+					{text:"发现",icon:"fa fa-smile-o",link:'/things'},
+					// {text:"发现",icon:"fa fa-smile-o",link:'/prearticle'},
+					{text:"发布",icon:"fa fa-pencil",link:'/designer'},
+					{text:"我的",icon:"fa fa-user-o",link:'/personal'}
 				]
 			}
 		},
